e61159218f4b77072b0fadf5658dcd2b82b4767a
cparraspecialized <camilo.parra@specialized.com>
2019-11-13 14:56:33 -0500

    Librerias de Excel y Lavachart

--- config/app.php
+++ config/app.php
@@ -148,7 +148,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,       	
         Barryvdh\DomPDF\ServiceProvider::class,
-        
+        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -214,6 +215,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,        	
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
